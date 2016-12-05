1b1f83abb27182e22a27975b2656bc7936e9b704
Khaled <k.alkiek@gmail.com>
2016-12-05 21:40:33 +0200

    Fix draw() function

--- cells.c
+++ cells.c
@@ -22,14 +22,28 @@ void draw() {
     static int i,j;
     printf("  ");
     for ( j = 0 ; j < c ; j++) {
-        printf("%2d ",j);
+        printf(" %3d",j); //print column numbers
     }
     printf("\n");
+
+    for(j=0; j<c; j++){ //print first horizontal break
+        if(j==0) printf("   +"); //print an extra '+' at the beginning of the first row
+        printf("---+");
+    }
+    printf("\n");
+
     for (i = 0 ; i < r ; i++ ) {
-            printf("%2d", i);
+            printf("%2d ", i);   //print row number
         for (j = 0 ; j < c ; j++ ) {
-            printf(" %c",grid[i][j].show);
+            if(j==0) printf("|"); //print first vertical break in the row
+            printf("%2c |",grid[i][j].show); //print cell's char and a vertical break
+        }
+        printf("\n"); //new line after each row
+        for(j=0; j<c; j++){ //horizontal break after each row
+            if(j==0) printf("   +"); //print first '+'
+            printf("---+");
         }
         printf("\n");
     }
+
 }
