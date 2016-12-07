06ede3f00a25041bef1962f2ee6f12e3eef0a84f
Khaled <k.alkiek@gmail.com>
2016-12-07 13:47:00 +0200

    add some input validation

--- game.c
+++ game.c
@@ -5,7 +5,14 @@
 #include "cells.h"
 #include "game.h"
 
+extern int r,c;
+
 bool gameState=1;
+
+bool inRange(int max, int input){
+    if(input >=0 && input < max) return 1;
+    else return 0;
+}
 
 void clearScreen(void){
     system("cls");
@@ -14,13 +21,25 @@ void clearScreen(void){
 
 bool play(){
 
-    int row, col;
+    int rowIn, colIn;
+    bool wrongInput = 0;
     char action;
     do
     {
-        clearScreen(); printf("\n"); draw();
+        clearScreen(); printf("\n");
+        draw();
+        if(wrongInput){
+            puts("Wrong entry.");
+            wrongInput = 0;
+        }
         puts("Please enter your move in the form of ( row col action )");
-        scanf("%d %d %c", &row, &col, &action);
+        scanf("%d %d %c", &rowIn, &colIn, &action);
+        fflush(stdin);
+        if ( !( inRange(r,rowIn) && inRange(c, colIn ) ) ){
+            wrongInput = 1;
+            continue;
+        }
+        // we still need to check the actions, which is the easiest thing ever so we won't do it now :'D
 
     }
     while (gameState);
