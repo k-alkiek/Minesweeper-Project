d25770f71a0aff55ddcee62324a536d50eb191b1
EngHesham <heshammedhat5@gmail.com>
2016-12-11 08:48:09 +0200

    Adding flagCell and post-move messaging

--- game.c
+++ game.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <string.h>
 #include "cells.h"
 #include "game.h"
 
 #define CELL(row,col) grid[row][col]
 
+bool messageFlag = 0;
+char message[100];
+int flags = 0 ;
 extern int r,c;
 extern struct cell grid[100][100];
 
@@ -47,9 +50,21 @@ void openCell(int row, int col) {
     return;
     }
     if (CELL(row,col).mined) {
-        gameState = 0;//TODO: change gameState to enum laeter
+        gameState = 0;//TODO: change gameState to enum later
     }
 }
+void flagCell(int row, int col) {
+    if (CELL(row,col).flagged == 1) {
+        messageFlag = 1;
+        strcpy (message,"The cell is already flagged. You can unmark it using the action 'u'");
+        return;
+    }
+    CELL(row,col).flagged = 1;
+    CELL(row,col).show = 'F' ;
+    flags++;
+    return;
+}
+
 
 bool play(){
 
@@ -64,6 +79,10 @@ bool play(){
             puts("Wrong entry.");
             wrongInput = 0;
         }
+        if (messageFlag) {
+            puts(message);
+            messageFlag = 0 ;
+        }
         puts("Please enter your move in the form of ( row col action )");
         rowIn = -1 ; colIn = -1; action = 'z';  //invalid values
         scanf("%d %d %c", &rowIn, &colIn, &action); //TODO: Adjust input method for saving
@@ -76,8 +95,8 @@ bool play(){
             case 'O':
             case 'o': {openCell(rowIn, colIn); break;}
             case 'F':
-            case 'f':       //TODO
-            case 'Q':       //add
+            case 'f': {flagCell(rowIn, colIn); break;}
+            case 'Q':       //TODO : add
             case 'q':       //functions
             case 'U':       //later
             case 'u':
